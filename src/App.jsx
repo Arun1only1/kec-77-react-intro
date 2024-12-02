f5cda1882938ce2034f4cb526b5ddc61824a03e5
arun <arun1nly1@gmail.com>
2024-12-02 14:15:06 +0545

    intro

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from './Button';
+
+const App = () => {
+  return (
+    <section>
+      <p>Hello World</p>
+
+      <a href='https://www.google.com'>go to google</a>
+
+      <Button />
+    </section>
+  );
+};
+
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>send me</button>;
+};
+
+export default Button;
